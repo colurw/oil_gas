b5f57f3b0f9a13c386c97d3fcae6806ea85a707d
colurw <colurw@gmail.com>
2026-02-12 16:52:27 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Thursday, Feb 12, 2026 at 4:42 PM
+
+Thursday, Feb 12, 2026 at 12:03 AM
+
+Thursday, Feb 12 2026 at 12:02 AM
+
+Wed, 11 Feb 2026 at 11:57 PM
+
+Wed, February 11 2026 at 11:55 PM
+
+Feb, Feb 11 2026 at 11:54 PM
+
+Wed, Feb 11 2026 at 11:53 PM
+
+Wed, Feb 11, 2026 at 5:21 PM
+
+Wednesday, Feb 11, 2026 at 5:19 PM
+
+str(Wednesday)[:3], Feb 11, 2026 at 5:19 PM
+
+Tuesday, Feb 10, 2026 at 7:48 PM
+
+My latest top-line for README
+
+My latest top-line for README
+
+My latest top-line for README
+
+My new and more recent top-line for README
+
+My new top-line for README
+
---
+++ test.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+def get_datetime(dt: datetime | None = None) -> str:
+    """ Return a local date/time string """
+    dt = datetime.now(tz=ZoneInfo("Europe/London"))
+    time_str = dt.strftime("%I:%M %p").lstrip("0")
+
+    return f"{dt.strftime('%A, %b %d, %Y')} at {time_str}"
+
+
+def update_readme_top(text: str) -> None:
+    """ Insert text at the top of readme.md """
+    readme = Path(__file__).resolve().parent / "README.md"
+    text_block = text.rstrip() + "\n\n"
+    existing = readme.read_text(encoding="utf-8")
+    new = text_block + existing.lstrip()
+    readme.write_text(new, encoding="utf-8")
+
+
+if __name__ == "__main__":
+
+    time = get_datetime() 
+    update_readme_top(time)
